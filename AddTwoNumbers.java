81122f5f5c94d32ef65828c34b496342b5e151c0
Zaheer Hussain <zaheerhussain9620@gmail.com>
2026-03-03 07:19:44 +0000

    testing error detection

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,9 +3,7 @@ class AddTwoNumbers {
   public static void main(String[] args) {
     
     int first = 10;
-    int second = 20;
-
-    // add two numbers
+    int second = 40
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
